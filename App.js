7dbfa1e79d3727741c3fbe8fc0784a98b6bab4dd
Khushi Sharma <khushi@xaigi.tech>
2025-02-20 00:43:13 +0530

    Learned about bundler parcel ,dist ,npm ,npx and how to create-react-app, difference btw package.json and package-lock.json

--- App.js
+++ App.js
@@ -1,19 +1,9 @@
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World from React!"
-// );
-// console.log("1", heading);
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// console.log("root", root);
-// console.log("11", heading);
-// root.render(heading);
-// console.log(root.render(heading)); //undefined
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "im h1 tag "),
+    React.createElement("h1", {}, "im h1 tag khushi here "),
     React.createElement("h2", {}, "im h2 tag "),
   ]),
   React.createElement("div", { id: "child2" }, [
@@ -21,7 +11,5 @@ const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("h2", {}, "im h2 tag "),
   ]),
 ]);
-console.log("parent", parent);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
---
+++ README.md
@@ -0,0 +1,21 @@
+# React
+
+# Parcel
+  Documentation : (parceljs.org)
+- Created Dev Build
+- Local Server(host our app to the server)
+- Automatically refereshing the page : doing HMR ->Hot Module Replacement
+- How parcel is doing HMR => File Watching Algorithm - written in c++ (parcel keeps track of every save we are doing in project)
+- Give faster Builds(Development experience) - because it is also doing caching
+- Image optimization
+- Minification
+- Bundling
+- Compress
+- Consistant hashing
+- Code spliting
+- Differntial Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking -> remove unused code
+- Different dev and prod bundles
